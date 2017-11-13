ab498fd7bf1bf8f7ed5bd9714d5ed0aad06c1cfb
Ismaïl <nguyen.ismail@gmail.com>
2017-11-13 21:51:11 +0100

    Test for rental of a children movie

--- RentalMovies-Kata.Tests/CustomerTest.cs
+++ RentalMovies-Kata.Tests/CustomerTest.cs
@@ -6,6 +6,25 @@ namespace RentalMovies_Kata.Tests
     public class CustomerTest
     {
         [Test]
+        public void Should_Print_Statement_For_Regular_Movie_Rental()
+        {
+            // GIVEN
+            var customer = new Customer("toto");
+            var movie = new Movie("Diner de cons", Movie.Regular);
+            var rental = new Rental(movie, 1);
+            customer.AddRental(rental);
+
+            // WHEN
+            var statement = customer.Statement();
+
+            // THEN
+            Check.That(statement).IsEqualTo("Rental Record for toto\n" +
+                "\tDiner de cons\t2\n" +
+                "Amount owed is 2\n" +
+                "You earned 1 frequent renter points");
+        }
+
+        [Test]
         public void Should_Print_Statement_For_NewRelease_Movie_Rental()
         {
             // GIVEN
@@ -25,11 +44,11 @@ namespace RentalMovies_Kata.Tests
         }
 
         [Test]
-        public void Should_Print_Statement_For_Regular_Movie_Rental()
+        public void Should_Print_Statement_For_Children_Movie_Rental()
         {
             // GIVEN
             var customer = new Customer("toto");
-            var movie = new Movie("Diner de cons", Movie.Regular);
+            var movie = new Movie("Pokemon", Movie.Children);
             var rental = new Rental(movie, 1);
             customer.AddRental(rental);
 
@@ -38,8 +57,8 @@ namespace RentalMovies_Kata.Tests
 
             // THEN
             Check.That(statement).IsEqualTo("Rental Record for toto\n" +
-                "\tDiner de cons\t2\n" +
-                "Amount owed is 2\n" +
+                "\tPokemon\t1.5\n" +
+                "Amount owed is 1.5\n" +
                 "You earned 1 frequent renter points");
         }
     }
