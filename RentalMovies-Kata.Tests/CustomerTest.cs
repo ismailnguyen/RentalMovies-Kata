aa002f9102bbb4e5e57c81d97caffe8737f26d47
Ismaïl <nguyen.ismail@gmail.com>
2017-11-13 21:41:58 +0100

    Test for rental of a new released movie

--- RentalMovies-Kata.Tests/CustomerTest.cs
+++ RentalMovies-Kata.Tests/CustomerTest.cs
@@ -6,14 +6,22 @@ namespace RentalMovies_Kata.Tests
     public class CustomerTest
     {
         [Test]
-        public void Should()
+        public void Should_Print_Statement_For_()
         {
             // GIVEN
+            var customer = new Customer("toto");
+            var movie = new Movie("Transformers", Movie.NewRelease);
+            var rental = new Rental(movie, 1);
+            customer.AddRental(rental);
 
             // WHEN
+            var statement = customer.Statement();
 
             // THEN
-            Check.That(true).IsTrue();
+            Check.That(statement).IsEqualTo("Rental Record for toto\n" +
+                "\tTransformers\t3\n" + 
+                "Amount owed is 3\n" + 
+                "You earned 1 frequent renter points");
         }
     }
 }
